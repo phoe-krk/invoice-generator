51354008c5ae9048c8e1bf3c5842449ec976e852
kuba1 <kuba1803@ymail.com>
2016-03-07 15:18:15 +0100

    Plik startowy i kontroler

--- Server/src/Kontroler/Kontroler.java
+++ Server/src/Kontroler/Kontroler.java
@@ -1,3 +1,4 @@
+package Kontroler;
 
 public class Kontroler {
 }
--- Server/src/Kontroler/Start.java
+++ Server/src/Kontroler/Start.java
@@ -1,6 +1,10 @@
+package Kontroler;
+
+import Kontroler.Kontroler;
 
 public class Start {
     public static void main(String[] args) {
+
         Kontroler kontroler = new Kontroler();
     }
 }
