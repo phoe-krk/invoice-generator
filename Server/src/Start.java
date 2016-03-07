40a7da692d1843fd7ffd9a302da034731fa26fd5
kuba1 <kuba1803@ymail.com>
2016-03-07 15:10:14 +0100

    Plik startowy i kontroler

--- Server/src/Start.java
+++ Server/src/Start.java
@@ -1,9 +1,6 @@
-/**
- * Created by kuba1 on 2016-03-07.
- */
+
 public class Start {
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        Kontroler kontroler = new Kontroler();
     }
 }
