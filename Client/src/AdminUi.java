cd167fdcffb86057b31b4ee1640d43d1c10a2b05
Jakub Pamuła <jakub.pamula@student.uj.edu.pl>
2016-06-12 03:53:28 +0200

    Work on client program, updated use cases

--- Client/src/AdminUi.java
+++ Client/src/AdminUi.java
@@ -1,5 +1,22 @@
+import javax.swing.*;
+
 /**
  * Created by koden on 11/06/2016.
  */
 public class AdminUi {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTable table1;
+    private JButton buttonNewInvoice;
+    private JButton editInvoiceButton;
+    private JButton exportInvoiceButton;
+    private JButton deleteInvoiceButton;
+    private JTable table2;
+    private JButton newClientButton;
+    private JButton editClientButton;
+    private JButton deleteClientButton;
+    private JTable table3;
+    private JButton newProductButton;
+    private JButton editProductButton;
+    private JButton deleteProductButton;
 }
--- Shared/src/DataType.java
+++ Shared/src/DataType.java
@@ -2,5 +2,5 @@
  * Created by koden on 11/06/2016.
  */
 public enum DataType {
-    LOGIN,PRODUCT,CLIENT,INVOICE,ERROR
+    LOGIN,PRODUCT,CLIENT,INVOICE,ERROR,USER
 }
